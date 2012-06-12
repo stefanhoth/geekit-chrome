b5b0721b8e8e37c594c781cf5b008f0d666e4f70
Stefan Hoth <sh@jnamic.com>
2012-06-12 02:29:35 +0200

    First working version

---
+++ README.md
@@ -0,0 +1,16 @@
+# GeekIt for Chrome
+A ported bookmarklet to post geeky links to Geeklist communities and your profile.
+
+## Why?
+
+BECAUSE! It's cool to use Chrome extensions instead of bookmarklets because they're easier to access and it was fun to implement it.
+
+## How?
+"Stealing" it from the original bookmarklet done by the Geeklist team and heavily adapting it to make it work within the Chrome extension with manifest version 2.
+
+
+## Who?
+
+[Stefan Hoth](http://stefanhoth.de) is a freelance developer for everything web and mobile (but he hate's pushing div's around for the perfect layout). He lives in Berlin, Germany, is member of the oldest and greatest [hacker space c-base space station](http://c-base.org) and is always interesting to learn new stuff and meet interesting people. Also he's the [German Ambassador for Geeklist](http://geekli.st/stefanhoth) and you can use his [invite code to get into the private beta](http://geekli.st/stefanhoth/invite/992C6CFAE8).
+
+You can reach him trough his [GitHub profile](https://github.com/stefanhoth).
---
+++ bookmarklet.js
@@ -0,0 +1,57 @@
+(function (W, D) {
+    var longDelay = 750
+      , shortDelay = 500
+      , frame = 'gklst-frame'
+      , loading = 'gklst-loading'
+      , style = 'gklst-style'
+      , host = 'http://geekli.st/'
+      , relink = W.context && W.context.link ? context.link.slug : false
+      , $
+
+  function createFrame () {
+  
+    chrome.tabs.getSelected(null,function(tab) {
+      console.log(tab);
+
+      var query = 'url='+escape(tab.url)
+        , img = $('img')[0]
+      if (img && img.src) {
+        query += '&img='+escape(img.src)
+      }
+      if (relink) {
+        query += '&relink='+relink
+      }
+      query += '&title='+escape(tab.title)
+      $('#'+frame).append(
+          $("<iframe/>")
+            .css("display","none")
+            .attr("src",host+"link/modal/?"+query)
+            .html("Please Enable iFrames.")
+            .load(function(){
+              var F = jQuery('#'+frame);
+              F.find('.'+loading).hide();
+              F.animate({height:525}, shortDelay,function(){F.find('iframe').fadeIn()})
+            })
+          );
+    });
+
+    
+  }
+
+  function destroyFrame () {
+    window.close()
+  }
+
+  function main (jQuery) {
+    $ = jQuery
+
+    $(document).ready(function(){ 
+      createFrame(); 
+      $('#'+frame+' .close').click(destroyFrame)
+    });
+
+  }
+
+  main(jQuery)
+
+})(window, document)
